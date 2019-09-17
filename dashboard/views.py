8ae1c9d9248418766571011096acfe05e7f03a4d
m7salam <mabdelsalam911@gmail.com>
2019-09-17 14:09:19 +0800

    added email notification for soap sensor

--- dashboard/views.py
+++ dashboard/views.py
@@ -68,6 +68,17 @@ def index(request):
             # send_mail(subject, html_content, sender, recipient_list, fail_silently=True)
     except:
         pass
+    try:
+        if percentage_soap <= 30.00:
+
+            subject = 'Alert your Soap are about to finish'
+            html_content = '<p>Sunway Toilet 1 Soap Holder is finishing. Please refill</p>'
+            sender = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [user_email]
+            send_html_mail(subject, html_content, recipient_list, sender)
+            # send_mail(subject, html_content, sender, recipient_list, fail_silently=True)
+    except:
+        pass
 
     return render(request, 'index.html', context)
 
